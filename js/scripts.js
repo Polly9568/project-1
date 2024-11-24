d767be10e4a41953777a0375d7563ecfbbd9406e
Polly <145790151+Polly9568@users.noreply.github.com>
2024-11-24 23:01:21 +0700

    objects declaration
    3 objects declaration with 3 keys which are name, height and types

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// objects, keys, arrays > declaration
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        types: ['fire', 'steel']
+    },
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        types: ['electric', 'psychic']
+     }
+    ];
